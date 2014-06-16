749730303878d17ddc416d9016538c687874fdaa
Justice <ebesco@yahoo.com>
2014-06-15 22:40:37 -0400

    setup a working theme with blog and front page

---
+++ footer.php
@@ -0,0 +1,12 @@
+<hr>
+
+      <footer>
+        <p><a href="<?php echo site_url(); ?>">&copy; <?php echo bloginfo('name'); ?></a></p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
